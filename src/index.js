6fe038b445a5554d90aa4bafded53969524f1cfa
chris <me@chriz.dev>
2017-02-21 19:55:21 +0800

    initial logic
    - does nothing yet

--- src/index.js
+++ src/index.js
@@ -1,45 +1,24 @@
-import { Component } from 'react'
+import { Component } from "react";
+import featureTest from "@thisguychris/css-feature-test";
 
-function featureTest( property, value, noPrefixes ) {
-  var prop = property + ':',
-      el = document.createElement( 'test' ),
-      mStyle = el.style;
-  
-  if( !noPrefixes ) {
-      mStyle.cssText = prop + [ '-webkit-', '-moz-', '-ms-', '-o-', '' ].join( value + ';' + prop ) + value + ';';
-  } else {
-      mStyle.cssText = prop + value;
-  }    
-  return mStyle[ property ];
-}
+const stickySupported = featureTest("position", "sticky");
 
 class Sticky extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      position: !!featureTest('position', 'sticky') ? 'sticky' : 'fixed'
-    }
   }
 
   render() {
-    style = {
-      top: this.props.top
-      , position: 'sticky'
-    }
-
     return (
-      <div style={ 
-        position: this.state.position
-        , top: this.props.top 
-        }>
-        { this.props.children }
-        </div>
-    )
+      <div style={{ position: this.state.position, top: this.props.top }}>
+        {this.props.children}
+      </div>
+    );
   }
 }
 
 Sticky.defaultProps = {
   top: 1
-}
+};
 
-export default Sticky
+export default Sticky;
